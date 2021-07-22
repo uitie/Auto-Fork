f7b6452812edab1ceb67898d8ba52b5dbaa1bad0
SYu449 <sy449@drexel.edu>
2021-07-22 00:05:41 -0400

    Removed line

--- auth_key.js
+++ auth_key.js
@@ -1,8 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('key_button').addEventListener('click', onClick, false)
     function onClick () {
-        console.log('test')
         const token = prompt('Enter Access Token')
-        console.log(token)
     }
 }, false)
